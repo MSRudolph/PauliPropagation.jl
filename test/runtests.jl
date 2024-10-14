708d6e6fa32331f60dfd8d496adc998776d71284
MSRudolph <manuel.rudolph@web.de>
2024-10-14 17:51:03 +0200

    add test for noise channels

--- test/runtests.jl
+++ test/runtests.jl
@@ -17,4 +17,7 @@ using Test
     @test hybridPP(8, 4, Inf, 0.0, Inf) ≈ 0.21720058439757214
     @test surrogatePP(8, 4, Inf, Inf) ≈ 0.21720058439757214
 
+    include("test_noisechannels.jl")
+    @test paulinoise(8, 4, Inf, 0.0)
+
 end
