7388f97a5be85b0929eb1708af5dc4a64b15eaea
teng10 <adateng07@gmail.com>
2024-10-30 18:19:59 +0100

    Added custom truncation by dissipation assisted amplitude damping.

--- src/PauliPropagation.jl
+++ src/PauliPropagation.jl
@@ -71,6 +71,8 @@ export
     commutator
 
 include("truncations.jl")
+export 
+    customtruncatedampingcoeff
 
 include("Propagation/Propagation.jl")
 export
--- src/truncations.jl
+++ src/truncations.jl
@@ -49,3 +49,36 @@ function truncatesins(path_properties::PathProperties, max_sins::Real)
     return path_properties.nsins > max_sins
 end
 
+# Custom truncation function
+
+# Define the custom truncation functions by dissipation-assisted damping
+function truncatedampingcoeff(
+    pstr::PauliString, coeff::Float64, gamma::Float64, min_abs_coeff::Float64
+)
+  return abs(coeff) * exp(- gamma * countweight(pstr))  < min_abs_coeff
+end
+
+function customtruncatedampingcoeff(
+    pstr_dict, gamma::Float64, min_abs_coeff::Float64
+)
+"""
+Custom truncation function with dissipation-assisted damping of coefficients.
+
+Truncate Pauli strings with coefficients < `min_abs_coeff` and damping `gamma`:
+c(P) * exp(- gamma * w(P)) < `min_abs_coeff`
+
+Args:
+    pstr_dict (dict): Dictionary of Pauli strings.
+    gamma (float): Damping gamma.
+    min_abs_coeff (float): Minimal absolute value of the coefficient.
+
+Returns:
+    None: Changes `pstr_dict` in-place.
+"""
+  for (pstr, coeff) in pstr_dict
+      if truncatedampingcoeff(pstr, coeff, gamma, min_abs_coeff)
+          delete!(pstr_dict, pstr)
+      end
+  end
+  return
+end
