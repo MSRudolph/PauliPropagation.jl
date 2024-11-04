3e499515ade1dfbefa33f7be339e2c2621ff1432
MSRudolph <manuel.rudolph@web.de>
2024-11-04 11:04:56 +0100

    clean up and fix bug with swapped coeff and theta

--- src/numericalcertificates.jl
+++ src/numericalcertificates.jl
@@ -1,10 +1,35 @@
 using Statistics
 
+
+
+"""
+Function to estimate the average error of a truncated circuit simulation using Monte Carlo sampling.
+Returns the mean squared error of the truncated Pauli propagation simulation averaged over all parameters.
+
+The function takes a circuit, a PauliString, and the number of Monte Carlo samples. Thetas will be set to ones and split_probabilities to 0.5 by default.
+Importantly, the `kwargs` can be used to set the truncation parameters of the Pauli propagation.
+"""
+function estimateaverageerror(circ, pstr::PauliString, n_mcsamples::Integer; circuit_reversed=false, kwargs...)
+    # length(thetas) should be equal to the number of parametrized gates in the circuit
+    thetas = ones(countparameters(circ))
+    # length(split_probabilities) should be equal to the number of total gates in the circuit
+    split_probabilities = ones(length(circ)) .* 0.5
+
+    outcome_array = zeros(n_mcsamples)
+
+    # TODO: Need to provide initial state overlapfunction
+
+    return estimateaverageerror!(circ, pstr, outcome_array, thetas, split_probabilities; circuit_reversed=circuit_reversed, kwargs...)
+
+end
+
+
+
 """
 Function to estimate the average error of a truncated circuit simulation using Monte Carlo sampling.
 Returns the mean squared error of the truncated Pauli propagation simulation averaged over `thetas`.
 
-The function takes a circuit, a PauliString, an array of thetas, an array of split_probabilities, and the number of Monte Carlo samples. 
+The function takes a circuit, a PauliString, the number of Monte Carlo samples, an array of thetas, and an array of split_probabilities. 
 Importantly, the `kwargs` can be used to set the truncation parameters of the Pauli propagation.
 
 The length the `thetas` vector should be equal to the number of parametrized gates in the circuit. This can be awkward, for example for Pauli gates, 
@@ -13,7 +38,7 @@ because they have parameters but in the average error estimation they are not us
 The the length of the `split_probabilities` vector should be equal to the number of total gates in the circuit. 
 This can be awkward, for example for non-splitting gates. But these matter for the splitting gates.
 """
-function estimateaverageerror(circ, pstr::PauliString, thetas::AbstractArray, split_probabilities::AbstractArray, n_mcsamples::Integer; kwargs...)
+function estimateaverageerror(circ, pstr::PauliString, n_mcsamples::Integer, thetas::AbstractArray, split_probabilities::AbstractArray; circuit_reversed=false, kwargs...)
     # length(thetas) should be equal to the number of parametrized gates in the circuit
     @assert length(thetas) == countparameters(circ)
     # length(split_probabilities) should be equal to the number of total gates in the circuit
@@ -23,11 +48,15 @@ function estimateaverageerror(circ, pstr::PauliString, thetas::AbstractArray, sp
 
     # TODO: Need to provide initial state overlapfunction
 
-    return estimateaverageerror!(circ, pstr, thetas, split_probabilities, outcome_array; kwargs...)
+    return estimateaverageerror!(circ, pstr, outcome_array, thetas, split_probabilities; circuit_reversed=circuit_reversed, kwargs...)
 
 end
 
-function estimateaverageerror!(circ, pstr::PauliString, thetas::AbstractArray, split_probabilities::AbstractArray, outcome_array::AbstractVector; kwargs...)
+
+"""
+In-place version of `estimateaverageerror`. This function takes an array of length m_mcsamples as an argument and modifies it in-place.
+"""
+function estimateaverageerror!(circ, pstr::PauliString, outcome_array::AbstractVector, thetas::AbstractArray, split_probabilities::AbstractArray; circuit_reversed=false, kwargs...)
     # This function takes an outcome_array as an argument and modifies it in-place.
 
     # length(thetas) should be equal to the number of parametrized gates in the circuit
@@ -36,25 +65,27 @@ function estimateaverageerror!(circ, pstr::PauliString, thetas::AbstractArray, s
     @assert length(split_probabilities) == length(circ)
 
     # reverse the circuit once 
-    circ = reverse(circ)
+    if circuit_reversed
+        circ = reverse(circ)
+    end
 
     n_mcsamples = length(outcome_array)
     @threads for ii in 1:n_mcsamples
-        coeff, final_operator, is_valid = montecarlopropagation(circ, pstr, thetas, split_probabilities; is_reversed=true, kwargs...)
+        final_pstr, is_valid = montecarlopropagation(circ, pstr, thetas, split_probabilities; circuit_reversed=true, kwargs...)
 
         # multiply the coefficient of the backpropagated Pauli with the overlap with the initial state (#TODO: provide initial state)
         # and then multply with (1 - is_valid) to get the final outcome.
         # if not valid, then the error is coeff * overlapwithzero(final_operator), if valid, then the error is 0
-        outcome_array[ii] = coeff * overlapwithzero(final_operator) * !is_valid
+        outcome_array[ii] = final_pstr.coeff * overlapwithzero(final_pstr.operator) * (1 - is_valid)
     end
 
     return mean(outcome_array)
 
 end
 
-function montecarlopropagation(circ, pstr::PauliString, thetas::AbstractArray, split_probabilities::AbstractArray; is_reversed=false, max_weight=Inf, max_freq=Inf, max_sins=Inf, kwargs...)
+function montecarlopropagation(circ, pstr::PauliString, thetas::AbstractArray, split_probabilities::AbstractArray; circuit_reversed=false, max_weight=Inf, max_freq=Inf, max_sins=Inf, kwargs...)
     # Reverse the circ if it is not already done. Allocates memory.
-    if !is_reversed
+    if circuit_reversed
         circ = reverse(circ)
     end
     param_idx = length(thetas)
@@ -62,27 +93,20 @@ function montecarlopropagation(circ, pstr::PauliString, thetas::AbstractArray, s
 
     is_valid = true
 
-    oper = pstr.operator
+    pauli = pstr.operator
     coeff = pstr.coeff
     for gate in circ
-        coeff, oper = mcapply(gate, oper, coeff, thetas[param_idx], split_probabilities[prob_idx]; kwargs...)
+
+        coeff, pauli = mcapply(gate, pauli, coeff, thetas[param_idx], split_probabilities[prob_idx]; kwargs...)
 
         # check truncations
         # TODO: make this properly
-        if truncateweight(oper, max_weight)
+        if truncateweight(pauli, max_weight)
             is_valid = false
-            oper = typeof(oper)(0)
-            break
-        end
-        if truncatefrequency(coeff, max_freq)
+        elseif truncatefrequency(coeff, max_freq)
             is_valid = false
-            oper = typeof(oper)(0)
-            break
-        end
-        if truncatesins(coeff, max_sins)
+        elseif truncatesins(coeff, max_sins)
             is_valid = false
-            oper = typeof(oper)(0)
-            break
         end
 
         # TODO: Custom truncation functions?
@@ -94,24 +118,24 @@ function montecarlopropagation(circ, pstr::PauliString, thetas::AbstractArray, s
         # always decrement the probability index
         prob_idx -= 1
     end
-    return coeff, oper, is_valid
+    return PauliString(pstr.nqubits, pauli, coeff), is_valid
 end
 
 # if we don't define an mcapply() function, assume that it doesn't split and that the apply function does the job
-mcapply(gate, oper, theta, coeff=1.0, args...) = apply(gate, oper, theta, coeff)
+mcapply(gate, pauli, coeff, theta, args...) = apply(gate, pauli, theta, coeff) # TODO: have more sensible default functions once the numerical certificate is figured out.
 
 # mcapply for Pauli gates
-function mcapply(gate::PauliGateUnion, oper, theta, coeff=1.0, split_prob=0.5)  # 
+function mcapply(gate::PauliGateUnion, pauli, coeff, theta, split_prob=0.5)  # 
 
-    if !commutes(gate, oper)
+    if !commutes(gate, pauli)
         # if the gate does not commute with the operator, split with probability split_prob int a random direction.
         if rand() > split_prob
             # TODO: make sure this works in general and not just for numerical coefficients. For example, add to freq and nsins.
-            sign, oper = getnewoperator(gate, oper)
+            sign, pauli = getnewoperator(gate, pauli)
         end
     end
     # the sign has abs(sign) = 1, so updating the coefficient with abs(sign) doesn't do anything
-    return coeff, oper
+    return coeff, pauli
 end
 
 function mcapply(gate::AmplitudeDampingNoise, args...)  # 
