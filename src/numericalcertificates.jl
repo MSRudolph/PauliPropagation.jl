4eaf3baebf1442541f0e39b797a9de754f4b9192
MSRudolph <manuel.rudolph@web.de>
2024-11-04 16:02:40 +0100

    small rework to simplify user-interface. removed automated calculation of split probabilities.

--- src/numericalcertificates.jl
+++ src/numericalcertificates.jl
@@ -3,60 +3,54 @@ using Statistics
 
 
 """
-Function to estimate the average error of a truncated circuit simulation using Monte Carlo sampling.
-Returns the mean squared error of the truncated Pauli propagation simulation averaged over all parameters.
+Function to estimate the average error of a truncated Pauli propagation simulation using Monte Carlo sampling.
+This version of the function is only valid for a circuit consisting of `PauliGates` non-parametrized non-splitting gates (for example Cliffords).
+Returns the mean squared error of the truncated Pauli propagation simulation averaged over the angles of all `PauliGate`s in the interval [-pi, pi].
 
-The function takes a circuit, a PauliString, and the number of Monte Carlo samples. Thetas will be set to ones and split_probabilities to 0.5 by default.
+An initial state overlap function `initialstatefunc` can be provided to calculate the overlap of the backpropagated Pauli operators with the initial state.
 Importantly, the `kwargs` can be used to set the truncation parameters of the Pauli propagation.
 """
 function estimateaverageerror(circ, pstr::PauliString, n_mcsamples::Integer; initialstatefunc=overlapwithzero, circuit_reversed=false, kwargs...)
-    # length(thetas) should be equal to the number of parametrized gates in the circuit
-    thetas = ones(countparameters(circ))
-    # length(split_probabilities) should be equal to the number of total gates in the circuit
-    split_probabilities = ones(length(circ)) .* 0.5
+    # this function is only valid for PauliGates and non-splitting non-parametrized gates
+    # this will not not error for non-parametrized splitting gates, e.g. T-gates. 
+    @assert all(g -> isa(g, PauliGateUnion) || isa(g, StaticGate), circ)
 
-    outcome_array = zeros(n_mcsamples)
+    # integration range over thetas is [-pi, pi]
+    thetas = ones(countparameters(circ)) * pi
 
-    # TODO: Need to provide initial state overlapfunction
-
-    return estimateaverageerror!(circ, pstr, outcome_array, thetas, split_probabilities; initialstatefunc=initialstatefunc, circuit_reversed=circuit_reversed, kwargs...)
-
+    return estimateaverageerror(circ, pstr, n_mcsamples, thetas; initialstatefunc=initialstatefunc, circuit_reversed=circuit_reversed, kwargs...)
 end
 
 
 
 """
 Function to estimate the average error of a truncated circuit simulation using Monte Carlo sampling.
-Returns the mean squared error of the truncated Pauli propagation simulation averaged over `thetas`.
+Returns the mean squared error of the truncated Pauli propagation simulation averaged over the `thetas`∈ [theta, theta] of the angle `theta` of each `PauliGate`.
 
-The function takes a circuit, a PauliString, the number of Monte Carlo samples, an array of thetas, and an array of split_probabilities. 
+The length the `thetas` vector should be equal to the number of parametrized gates in the circuit. For `PauliGates`, the value should be the integration range of the parameters around zero.
+For other currently supported parametrized gates, potential splitting probabilities can be derived from the parameters (e.g., for `AmplitudeDampingNoise`). 
+We currently support no non-parametrized splitting gates. This may change in the future.
+
+An initial state overlap function `initialstatefunc` can be provided to calculate the overlap of the backpropagated Pauli operators with the initial state.
 Importantly, the `kwargs` can be used to set the truncation parameters of the Pauli propagation.
-
-The length the `thetas` vector should be equal to the number of parametrized gates in the circuit. This can be awkward, for example for Pauli gates, 
-because they have parameters but in the average error estimation they are not used. But the thetas matter for non-splitting parametrized gates.
-
-The the length of the `split_probabilities` vector should be equal to the number of total gates in the circuit. 
-This can be awkward, for example for non-splitting gates. But these matter for the splitting gates.
 """
-function estimateaverageerror(circ, pstr::PauliString, n_mcsamples::Integer, thetas::AbstractArray, split_probabilities::AbstractArray; initialstatefunc=overlapwithzero, circuit_reversed=false, kwargs...)
+function estimateaverageerror(circ, pstr::PauliString, n_mcsamples::Integer, thetas::AbstractArray; initialstatefunc=overlapwithzero, circuit_reversed=false, kwargs...)
     # length(thetas) should be equal to the number of parametrized gates in the circuit
     @assert length(thetas) == countparameters(circ)
-    # length(split_probabilities) should be equal to the number of total gates in the circuit
-    @assert length(split_probabilities) == length(circ)
+
+    split_probabilities = _calculatesplitprobabilities(circ, thetas)
 
     outcome_array = zeros(n_mcsamples)
-
-    # TODO: Need to provide initial state overlapfunction
 
     return estimateaverageerror!(circ, pstr, outcome_array, thetas, split_probabilities; initialstatefunc=initialstatefunc, circuit_reversed=circuit_reversed, kwargs...)
 
 end
 
-
 """
-In-place version of `estimateaverageerror`. This function takes an array of length m_mcsamples as an argument and modifies it in-place.
+In-place version of `estimateaverageerror`. This function takes an array of length n_mcsamples as an argument and modifies it in-place. 
+It further assumes that the `thetas` and `split_probabilities` are already correctly calculated and provided as arguments.
 """
-function estimateaverageerror!(circ, pstr::PauliString, outcome_array::AbstractVector, thetas::AbstractArray, split_probabilities::AbstractArray; initialstatefunc=overlapwithzero, circuit_reversed=false, kwargs...)
+function estimateaverageerror!(circ, pstr::PauliString, outcome_array::AbstractVector, thetas::AbstractArray, split_probabilities::AbstractArray; initialstatefunc=paulioverlapwithzero, circuit_reversed=false, kwargs...)
     # This function takes an outcome_array as an argument and modifies it in-place.
 
     # length(thetas) should be equal to the number of parametrized gates in the circuit
@@ -124,11 +118,14 @@ end
 # if we don't define an mcapply() function, assume that it doesn't split and that the apply function does the job
 mcapply(gate, pauli, coeff, theta, split_probability; kwargs...) = apply(gate, pauli, theta, coeff; kwargs...) # TODO: have more sensible default functions once the numerical certificate is figured out.
 
-# mcapply for Pauli gates
+## Monte Carlo apply functions
+"""
+MC apply function for Pauli gates. If the gate commutes with the operator, the operator is left unchanged. Else the operator is split off with a probability 1 - `split_prob`.
+"""
 function mcapply(gate::PauliGateUnion, pauli, coeff, theta, split_prob=0.5; kwargs...)  # 
 
     if !commutes(gate, pauli)
-        # if the gate does not commute with the operator, split with probability split_prob int a random direction.
+        # if the gate does not commute with the operator, remain with probability `split_prob` and split off with probability 1 - `split_prob`.
         if rand() > split_prob
             # TODO: make sure this works in general and not just for numerical coefficients. For example, add to freq and nsins.
             sign, pauli = getnewoperator(gate, pauli)
@@ -140,5 +137,33 @@ end
 
 function mcapply(gate::AmplitudeDampingNoise, args...; kwargs...)  # 
     # TODO
-    println("AmplitudeDampingNoise not implemented yet")
+    throw("AmplitudeDampingNoise not implemented yet")
+end
+
+
+
+### Utilities
+"""
+Function that automatically calculates the splitting probabilities of the gates in the circuit based on a vector of thetas.
+For Pauli gates, the theta value is interpreted as the limits of the integration [-theta, theta].
+For AmplitudeDampingNoise, the splitting probability is the damping rate.
+"""
+function _calculatesplitprobabilities(circ, thetas)
+    split_probabilities = zeros(length(circ))
+
+    theta_idx = 1
+    for (ii, gate) in enumerate(circ)
+        if isa(gate, PauliGateUnion)
+            # integration range
+            r = thetas[theta_idx]
+            split_probabilities[ii] = 0.5 * (1 + sin(2r) / (2r))
+        elseif isa(gate, AmplitudeDampingNoise)
+            # probability of splitting = damping rate. Need to rescale diagonal accordingly.
+            split_probabilities[ii] = thetas[theta_idx]
+        end
+        if isa(gate, ParametrizedGate)
+            theta_idx += 1
+        end
+    end
+    return split_probabilities
 end
