769f2a120766a03f8455d513ab39c9568555ad68
Tyson Jones <tyson.jones.input@gmail.com>
2025-04-09 16:28:55 +0200

    Scaffold Documenter.jl doc

---
+++ docs/make.jl
@@ -0,0 +1,11 @@
+using Documenter, PauliPropagation
+
+makedocs(
+    sitename="Pauli Propagation",
+    pages=[
+        "introduction.md",
+        "installation.md",
+        "tutorials.md",
+        "index.md",
+    ]
+)
