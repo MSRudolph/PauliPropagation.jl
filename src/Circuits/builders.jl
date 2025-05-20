f6fc36464e9afe22d6de5d8a25ece4acf8964861
MSRudolph <manuel.rudolph@web.de>
2025-05-20 16:42:14 +0200

    add Hamiltonian equations for circuit builders

--- src/Circuits/builders.jl
+++ src/Circuits/builders.jl
@@ -13,6 +13,7 @@ Create a circuit that corresponds to a Trotterization of the transverse-field Is
 ```math
 H = ∑_{(i,j) ∈ topology} J_{ij} Z_i Z_j + ∑_{i=1 ... nqubits} h_i X_i
 ```
+This returns only the sequence of gates, not their parameters.
 A topology can be specified as a list of pairs of qubit indices. If no topology is specified, a bricklayer topology is used.
 If `start_with_ZZ` is set to `true`, the circuit starts with a layer of ZZ gates, else with a layer of X gates. This is relevant depending on the initial state.
 """
@@ -50,6 +51,7 @@ Create a circuit that corresponds to a Trotterization of the transverse-field Is
 H = ∑_{(i,j) ∈ topology} J_{ij} Z_i Z_j 
   + ∑_{i=1 ... nqubits} h_i Z_i +  ∑_{i=1 ... nqubits} b_i X_i
 ```
+This returns only the sequence of gates, not their parameters.
 A topology can be specified as a list of pairs of qubit indices. If no topology is specified, a bricklayer topology is used.
 """
 function tiltedtfitrottercircuit(nqubits::Integer, layers::Integer; topology=bricklayertopology(nqubits))
@@ -70,8 +72,14 @@ end
     heisenbergtrottercircuit(nqubits::Integer, nlayers::Integer; topology=bricklayertopology(nqubits))
 
 Create a circuit that corresponds to a Trotterization of the Heisenberg Hamiltonian.
+```math
+H = ∑_{(i,j) ∈ topology} Jzz_{ZZ} Z_i Z_j 
++ ∑_{(i,j) ∈ topology} Jxx_{XX} X_i X_j
++ ∑_{(i,j) ∈ topology} Jyy_{YY} Y_i Y_j
+```
+This returns only the sequence of gates, not their parameters.
 A topology can be specified as a list of pairs of qubit indices. If no topology is specified, a bricklayer topology is used.
-Note that the gates are applied as layers of XX-YY-ZZ gates, not as layers of XX on all, then YY on all, then ZZ on all. On the bricklayer topology, these are equivalent.
+Note that the gates are applied as layers of XX-YY-ZZ gates, not as layers of XX on all, then YY on all, then ZZ on all.
 """
 function heisenbergtrottercircuit(nqubits::Integer, nlayers::Integer; topology=bricklayertopology(nqubits))
     circuit::Vector{Gate} = []
