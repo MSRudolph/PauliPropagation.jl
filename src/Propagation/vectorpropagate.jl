740e4dd9b55c9f23968ad506053eb17dc76da766
MSRudolph <manuel.rudolph@web.de>
2025-10-19 18:34:46 +0200

    first version that works on GPU

--- src/Propagation/vectorpropagate.jl
+++ src/Propagation/vectorpropagate.jl
@@ -1,5 +1,6 @@
 using Base.Threads
 using AcceleratedKernels
+const AK = AcceleratedKernels
 using PauliPropagation
 
 struct WeightedString{TT,CT}
@@ -7,24 +8,78 @@ struct WeightedString{TT,CT}
     coeff::CT
 end
 
+termtype(wstring::WeightedString{TT,CT}) where {TT,CT} = TT
+coefftype(wstring::WeightedString{TT,CT}) where {TT,CT} = CT
 
-# TODO: an is_sorted flag?
-struct PropagationCache{TT,CT}
-    terms::Vector{WeightedString{TT,CT}}
-    flags::Vector{Bool}
-    indices::Vector{Int}
+Base.:*(wstring::WeightedString, c) = WeightedString(wstring.term, wstring.coeff * c)
 
-    function PropagationCache(terms::Vector{WeightedString{TT,CT}}, flags, indices) where {TT,CT}
-        @assert length(terms) == length(flags) == length(indices)
-        new{TT,CT}(terms, flags, indices)
-    end
+# types are:
+# vector of terms 
+# vector of flags (bool)
+# vector of indices (int)
+mutable struct PropagationCache{VT,VB,VI}
+    terms::VT
+    aux_terms::VT
+    flags::VB
+    indices::VI
+
+    # we will over-allocate the arrays and keep track of the non-empty size
+    active_size::Int
 end
 
-function PropagationCache(strings::Vector)
-    n = length(strings)
-    flags = Vector{Bool}(undef, n)
-    indices = Vector{Int}(undef, n)
-    return PropagationCache(strings, flags, indices)
+function Base.resize!(prop_cache::PropagationCache, n_new::Int)
+    resize!(prop_cache.terms, n_new)
+    resize!(prop_cache.aux_terms, n_new)
+    resize!(prop_cache.flags, n_new)
+    resize!(prop_cache.indices, n_new)
+    return prop_cache
+end
+
+function PropagationCache(terms)
+    aux_terms = similar(terms)
+    flags = similar(terms, Bool)
+    indices = similar(terms, Int)
+    return PropagationCache(terms, aux_terms, flags, indices, length(terms))
+end
+
+termtype(prop_cache::PropagationCache{VT,VB,VI}) where {VT,VB,VI} = eltype(VT).parameters[1]
+coefftype(prop_cache::PropagationCache{VT,VB,VI}) where {VT,VB,VI} = eltype(VT).parameters[2]
+
+viewterms(prop_cache::PropagationCache) = view(prop_cache.terms, 1:prop_cache.active_size)
+viewauxterms(prop_cache::PropagationCache) = view(prop_cache.aux_terms, 1:prop_cache.active_size)
+viewflags(prop_cache::PropagationCache) = view(prop_cache.flags, 1:prop_cache.active_size)
+viewindices(prop_cache::PropagationCache) = view(prop_cache.indices, 1:prop_cache.active_size)
+
+Base.length(prop_cache::PropagationCache) = prop_cache.active_size
+Base.isempty(prop_cache::PropagationCache) = length(prop_cache) == 0
+
+function swapterms!(prop_cache::PropagationCache)
+    prop_cache.terms, prop_cache.aux_terms = prop_cache.aux_terms, prop_cache.terms
+    return prop_cache
+end
+
+function zeroinactive!(prop_cache::PropagationCache)
+    TT = termtype(prop_cache)
+    CT = coefftype(prop_cache)
+
+    AK.foreachindex(prop_cache.terms) do ii
+        if ii > prop_cache.active_size
+            prop_cache.terms[ii] = WeightedString(zero(TT), zero(CT))
+        end
+    end
+    return prop_cache
+end
+
+function topaulisum(nq, prop_cache::PropagationCache)
+    TT = termtype(prop_cache)
+    CT = coefftype(prop_cache)
+    # return PauliSum(nq, Dict(s.term => s.coeff for s in strings))
+    psum = PauliSum(CT, nq)
+    sizehint!(psum.terms, length(prop_cache.active_size))
+    for wstring in viewterms(prop_cache)
+        add!(psum, wstring.term, wstring.coeff)
+    end
+    return psum
 end
 
 
@@ -32,7 +87,7 @@ function vectorpropagate(circuit, strings::Vector; kwargs...)
     return vectorpropagate(circuit, PropagationCache(strings); kwargs...).terms
 end
 
-function vectorpropagate(circuit, prop_cache::PropagationCache; min_abs_coeff=1e-10, kwargs...)
+function vectorpropagate(circuit, prop_cache::PropagationCache; min_abs_coeff=1e-10, max_weight=Inf, kwargs...)
     # assume circuit contains the parameters via freezing, or is parameter-free
     @assert countparameters(circuit) == 0 "'circuit' must be parameter-free. Consider using 'freeze()'."
 
@@ -40,9 +95,12 @@ function vectorpropagate(circuit, prop_cache::PropagationCache; min_abs_coeff=1e
     for (i, gate) in enumerate(reverse(circuit))
 
         prop_cache = applygate!(gate, prop_cache)
+        # println("Done with applygate!()")
 
         prop_cache = mergeterms!(prop_cache)
-        prop_cache = truncate!(prop_cache, min_abs_coeff)
+        # println("Done with mergeterms!()")
+        prop_cache = truncate!(prop_cache; min_abs_coeff, max_weight)
+        # println("Done with truncate!()")
 
         ## Thoughts:
         # - Can we merge and truncate in one?
@@ -51,6 +109,7 @@ function vectorpropagate(circuit, prop_cache::PropagationCache; min_abs_coeff=1e
 
 
     end
+    # TODO: zero-out inactive parts so people are not confused
     return prop_cache
 end
 
@@ -66,7 +125,7 @@ end
 function applygate!(gate::CliffordGate, prop_cache::PropagationCache)
 
     # everything is done in place
-    @threads for ii in eachindex(prop_cache.terms)
+    AK.foreachindex(viewterms(prop_cache)) do ii
         wstring = prop_cache.terms[ii]
 
         term, coeff = apply(gate, wstring.term, wstring.coeff)
@@ -77,9 +136,27 @@ function applygate!(gate::CliffordGate, prop_cache::PropagationCache)
     return prop_cache
 end
 
-function applygate!(frozen_gate::FrozenGate{PauliRotation,PT}, prop_cache::PropagationCache{TT,CT}) where {CT,TT,PT}
 
-    n_oldstrings = length(prop_cache.terms)
+function PauliPropagation.getnewpaulistring(gate_mask::TT, pstr::TT) where TT
+    new_pstr = PauliPropagation._bitpaulimultiply(gate_mask, pstr)
+
+    # this counts the exponent of the imaginary unit in the new Pauli string
+    im_count = PauliPropagation._calculatesignexponent(gate_mask, pstr)
+
+    # now, instead of computing im^im_count followed by another im factor from the gate rules,
+    # we do this in one step via a cheeky trick:
+    sign = (im_count & 2) - 1
+    # this is equivalent to sign = real( im * im^im_count)
+
+    return new_pstr, sign
+end
+
+function applygate!(frozen_gate::FrozenGate{PauliRotation,PT}, prop_cache::PropagationCache) where {PT}
+
+    TT = termtype(prop_cache)
+    CT = coefftype(prop_cache)
+
+    n_old = length(prop_cache)
 
     # unpack the frozen PauliRotation
     gate, theta = frozen_gate.gate, frozen_gate.parameter
@@ -91,125 +168,162 @@ function applygate!(frozen_gate::FrozenGate{PauliRotation,PT}, prop_cache::Propa
     cos_val = cos(theta)
     sin_val = sin(theta)
 
-    @threads for ii in eachindex(prop_cache.terms)
-        wstring = prop_cache.terms[ii]
-        prop_cache.flags[ii] = !commutes(masked_gate, wstring.term)
+    # get the mask out because because the gate cannot be in the function when using GPU
+    gate_mask = masked_gate.generator_mask
+    anticommfunc = wstring -> anticommutes(wstring, gate_mask)
+    flagterms!(prop_cache, anticommfunc)
+
+    flagstoindices!(prop_cache)
+
+    n_noncommutes = prop_cache.indices[prop_cache.active_size]
+
+    # slit off into the same array
+    n_new = n_old + n_noncommutes
+    if length(prop_cache.terms) < n_new
+        resize!(prop_cache, n_new)
     end
 
-    # index_vector = cumsum(anticommutes_vector)
-    AcceleratedKernels.accumulate!(+, prop_cache.indices, prop_cache.flags; init=0)
-    n_noncommutes = prop_cache.indices[end]
-
-    # pre-allocate the output array
-    new_pauli_strings = Vector{WeightedString{TT,CT}}(undef, n_oldstrings + n_noncommutes)
-    # new_prop_cache = PropagationCache(new_pauli_strings)
-
-    @threads for ii in eachindex(prop_cache.terms)
-        wstring = prop_cache.terms[ii]
-
-        if prop_cache.flags[ii]
+    term_view = viewterms(prop_cache)
+    terms = prop_cache.terms
+    flags = prop_cache.flags
+    indices = prop_cache.indices
+    AK.foreachindex(term_view) do ii
+        # here it anticommutes
+        if flags[ii]
+            wstring = terms[ii]
             term = wstring.term
             coeff = wstring.coeff
 
             coeff1 = coeff * cos_val
-            new_term, sign = PauliPropagation.getnewpaulistring(masked_gate, term)
+            new_term, sign = PauliPropagation.getnewpaulistring(gate_mask, term)
             coeff2 = coeff * sin_val * sign
 
-            new_pauli_strings[ii] = WeightedString(term, coeff1)
-            new_pauli_strings[n_oldstrings+prop_cache.indices[ii]] = WeightedString(new_term, coeff2)
-
-        else
-            new_pauli_strings[ii] = wstring
+            terms[ii] = WeightedString(term, coeff1)
+            terms[n_old+indices[ii]] = WeightedString(new_term, coeff2)
         end
     end
+    prop_cache.active_size = n_new
 
-    # re-use the flags and indices arrays
-    resize!(prop_cache.flags, length(new_pauli_strings))
-    resize!(prop_cache.indices, length(new_pauli_strings))
-    return PropagationCache(new_pauli_strings, prop_cache.flags, prop_cache.indices)
+    return prop_cache
+end
+
+anticommutes(wstring::WeightedString{TT,CT}, gate_generator::TT) where {TT,CT} = anticommutes(wstring.term, gate_generator)
+anticommutes(term::TT, gate_generator::TT) where {TT} = !commutes(term, gate_generator)
+
+function flagterms!(prop_cache::PropagationCache, by::F) where F
+    term_view = viewterms(prop_cache)
+    flags = prop_cache.flags
+    AK.foreachindex(term_view) do ii
+        flags[ii] = by(term_view[ii])
+    end
+    return
 end
 
 term(wstring::WeightedString) = wstring.term
 
-function mergeterms!(prop_cache::PropagationCache{TT,CT}) where {TT,CT}
-    if isempty(prop_cache.terms)
+flagstoindices!(prop_cache::PropagationCache) = flagstoindices!(prop_cache.indices, viewflags(prop_cache))
+flagstoindices!(indices_dst, flags) = AK.accumulate!(+, indices_dst, flags; init=0)
+
+function mergeterms!(prop_cache::PropagationCache)
+    if isempty(prop_cache)
         return prop_cache
     end
 
-    n_terms = length(prop_cache.terms)
-
     # Sort the vector to group identical keys together.
-    AcceleratedKernels.sort!(prop_cache.terms, by=term)
+    AK.sort!(viewterms(prop_cache); by=term, temp=viewauxterms(prop_cache))
 
     # Find the start of each group in parallel.
-    prop_cache.flags[1] = true # The first element always starts a group.
-    @threads for i in 2:n_terms
-        # A new group starts if the term is different from the previous one.
-        prop_cache.flags[i] = prop_cache.terms[i].term != prop_cache.terms[i-1].term
+
+    term_view = viewterms(prop_cache)
+    flags = prop_cache.flags
+    AK.foreachindex(term_view) do ii
+        if ii == 1
+            flags[ii] = true
+        else
+            flags[ii] = term_view[ii].term != term_view[ii-1].term
+        end
     end
 
     # get the indices for where to add the coefficients
-    AcceleratedKernels.accumulate!(+, prop_cache.indices, prop_cache.flags; init=0)
-    n_unique_terms = prop_cache.indices[end]
+    flagstoindices!(prop_cache)
+    n_unique_terms = prop_cache.indices[prop_cache.active_size]
 
-    # create the new cache
-    merged_prop_cache = PropagationCache(typeof(prop_cache.terms)(undef, n_unique_terms))
-
-    # populate the start_indices vector
-    @threads for i in eachindex(prop_cache.terms)
-        if prop_cache.flags[i]
-            # `prop_cache.indices[i]` gives the 1-based index for this new group.
-            # We place the actual array index `i` into that slot.
-            merged_prop_cache.indices[prop_cache.indices[i]] = i
-        end
+    # early stop if all are unique 
+    if n_unique_terms == prop_cache.active_size
+        return prop_cache
     end
 
-    # sum over each group
-    @threads for i in 1:n_unique_terms
-        start_idx = merged_prop_cache.indices[i]
-        # The group ends right before the next group starts, or at the end of the array.
-        end_idx = (i < n_unique_terms) ? merged_prop_cache.indices[i+1] - 1 : n_terms
+    # the reason we don't do atomic add instead is because the entry might not start off as 0
+    deduplicate!(prop_cache)
 
-        # The term is the same for all elements in this range.
-        group_term = prop_cache.terms[start_idx].term
+    # swap terms and aux_terms
+    swapterms!(prop_cache)
+    prop_cache.active_size = n_unique_terms
 
-        # Sum the values in the range.
-        total_sum = zero(CT)
+    return prop_cache
+end
 
-        for j in start_idx:end_idx
-            total_sum += prop_cache.terms[j].coeff
+function deduplicate!(prop_cache::PropagationCache)
+
+    term_view = viewterms(prop_cache)
+    aux_terms = prop_cache.aux_terms
+    flags = prop_cache.flags
+    indices = prop_cache.indices
+    active_size = prop_cache.active_size
+    AK.foreachindex(term_view) do ii
+        # if this is the start of a new group
+        if flags[ii]
+            # end index is the before the next flag or the end of the array
+            end_idx = ii
+            while end_idx < active_size && !flags[end_idx+1]
+                end_idx += 1
+            end
+
+            # Sum the values in the range.
+            CT = typeof(term_view[ii].coeff)
+            merged_coeff = zero(CT)
+            for jj in ii:end_idx
+                merged_coeff += term_view[jj].coeff
+            end
+
+            aux_terms[indices[ii]] = WeightedString(term_view[ii].term, merged_coeff)
         end
-
-        merged_prop_cache.terms[i] = WeightedString(group_term, total_sum)
     end
-
-    return merged_prop_cache
+    return
 end
 
 
-function truncate!(prop_cache::PropagationCache{TT,CT}, min_abs_coeff::Real) where {TT,CT}
 
-    # flag the qubits that would not be deleted
-    @threads for ii in eachindex(prop_cache.terms)
-        pstr = prop_cache.terms[ii]
-        prop_cache.flags[ii] = abs(tonumber(pstr.coeff)) >= min_abs_coeff
+function truncate!(prop_cache::PropagationCache{TT,CT}; min_abs_coeff::Real, max_weight::Real=Inf) where {TT,CT}
+
+    if isempty(prop_cache)
+        return prop_cache
     end
+
+    # TODO: can we simplify this entire function via a parallel filter!() function?
+
+    # flag the qubits that we keep
+    flagterms!(prop_cache, wstring -> (abs(tonumber(wstring.coeff)) >= min_abs_coeff) && countweight(wstring.term) <= max_weight)
 
     # get the new indices after deletion
-    AcceleratedKernels.accumulate!(+, prop_cache.indices, prop_cache.flags; init=0)
-    n_kept = prop_cache.indices[end]
+    flagstoindices!(prop_cache)
+    n_kept = prop_cache.indices[prop_cache.active_size]
 
     # pre-allocate the output array
-    filtered_terms = Vector{WeightedString{TT,CT}}(undef, n_kept)
-    @threads for ii in eachindex(prop_cache.terms)
-        if prop_cache.flags[ii]
-            filtered_terms[prop_cache.indices[ii]] = prop_cache.terms[ii]
+    term_view = viewterms(prop_cache)
+    aux_terms = prop_cache.aux_terms
+    flags = prop_cache.flags
+    indices = prop_cache.indices
+    AK.foreachindex(term_view) do ii
+        if flags[ii]
+            aux_terms[indices[ii]] = term_view[ii]
         end
     end
-    # resize old arrays 
-    flags = resize!(prop_cache.flags, n_kept)
-    indices = resize!(prop_cache.indices, n_kept)
-    return PropagationCache(filtered_terms, flags, indices)
+
+    swapterms!(prop_cache)
+    prop_cache.active_size = n_kept
+
+    return prop_cache
 end
 
 function chopterms!(prop_cache::PropagationCache, max_terms)
@@ -243,7 +357,7 @@ function PauliPropagation.overlapwithzero(strings::Vector)
 end
 
 
-function PauliPropagation.overlapwithcomputational(strings::Vector{WeightedString{TT,CT}}, onebitinds) where {TT,CT}
+function PauliPropagation.overlapwithcomputational(strings::AbstractArray{WeightedString{TT,CT}}, onebitinds) where {TT,CT}
     val = zero(CT)
     for wstring in strings
         val += tonumber(wstring.coeff) * PauliPropagation._calcsignwithones(wstring.term, onebitinds)
