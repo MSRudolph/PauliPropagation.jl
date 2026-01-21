63e15af1d9f8eac8df0d49a11ce6b77f207de2ca
MSRudolph <manuel.rudolph@web.de>
2026-01-21 11:12:18 +0100

    propagate docstrings

--- src/Base/propagate.jl
+++ src/Base/propagate.jl
@@ -1,112 +1,135 @@
 
-# TODO: Once this is not typed, the old propagate() needs to be typed to ::PauliSum
 function propagate(circuit, term_sum::AbstractTermSum, parameters=nothing; kwargs...)
     return propagate!(circuit, deepcopy(term_sum), parameters; kwargs...)
 end
 
 
-function propagate!(circuit, term_sum::TS, parameters=nothing; kwargs...) where TS<:AbstractTermSum
-    prop_cache = propagate!(circuit, PropagationCache(term_sum), parameters; kwargs...)
+function propagate!(circuit, term_sum::TS, params=nothing; kwargs...) where TS<:AbstractTermSum
+    prop_cache = propagate!(circuit, PropagationCache(term_sum), params; kwargs...)
     # we expect that there exists a constructor TS(prop_cache) for the back conversion
-    try
-        return TS(prop_cache)
-    catch e
-        throw(ErrorException("Could not convert the propagation cache of type $(typeof(prop_cache)) back to the original TermSum type $(TS).
-         Make sure that a constructor $(TS)(::$(typeof(prop_cache))) is defined. Original error:\n $e"))
-    end
+    # by default implemented for some storage types
+    return TS(prop_cache)
 end
 
-function propagate!(circuit, prop_cache::AbstractPropagationCache, parameters=nothing; kwargs...)
+"""
+    propagate!(circuit, prop_cache::AbstractPropagationCache, params=nothing; kwargs...)
+
+Propagate a term sum through under the action of gates in `circuit` using a propagation cache `prop_cache`. 
+This is in-place and modifies `prop_cache` along with the term sums it carries.
+Parameters for the parametrized gates in `circuit` are given by `params`, and need to be passed as if the circuit was applied as written in the Schrödinger picture.
+If params are not passed, the circuit must contain only non-parametrized `StaticGates`.
+`kwargs` are passed to the lower-level functions `applymergetruncate!`, `applytoall!`, `applyandadd!`, and `apply`,
+as well as `merge!` and `truncate!`.
+Default truncation kwargs are `min_abs_coeff` and `customtruncfunc`.
+"""
+function propagate!(circuit, prop_cache::AbstractPropagationCache, params=nothing; kwargs...)
 
     # TODO: The two methods should be one
-    # if circ is actually a single gate, promote it to a list [gate]
-    # similarly the theta if it is a single number
-    circuit, parameters = _promotecircandthetas(circuit, parameters)
+    # if circuit is actually a single gate, promote it to a list [gate]
+    # similarly the params if it is a single number
+    circuit, params = _promotecircandparams(circuit, params)
 
-    # if thetas is nothing, the circuit must contain only StaticGates
-    # also check if the length of thetas equals the number of parametrized gates
-    _checkcircandthetas(circuit, parameters)
+    # if params is nothing, the circuit must contain only StaticGates
+    # also check if the length of params equals the number of parametrized gates
+    _checkcircandparams(circuit, params)
 
     # TODO: allow forward propagation 
     # For now we only assume backward propagation
     circuit = reverse(circuit)
-    parameters = reverse(parameters)
+    params = reverse(params)
 
     # A useful iteration tool
-    parameter_iterator = Iterators.Stateful(parameters)
+    parameter_iterator = Iterators.Stateful(params)
 
     for gate in circuit
         if isa(gate, ParametrizedGate)
             param = popfirst!(parameter_iterator)
-            prop_cache = applymergetruncate!(gate, prop_cache, param; kwargs...)
+            applymergetruncate!(gate, prop_cache, param; kwargs...)
         else
-            prop_cache = applymergetruncate!(gate, prop_cache; kwargs...)
+            applymergetruncate!(gate, prop_cache; kwargs...)
         end
     end
 
     return prop_cache
 end
 
+"""
+    applymergetruncate!(gate, prop_cache::AbstractPropagationCache; kwargs...)
+    applymergetruncate!(gate, prop_cache::AbstractPropagationCache, parameter; kwargs...)
 
+1st-level function below `propagate!` that applies one gate to all terms in the main term sum `mainsum(prop_cache)`, 
+potentially using an auxiliary term sum `auxsum(prop_cache)` in the process. 
+All terms are then merged and deduplicated into the main term sum.
+Truncations are performed after merging.
+This function can be overwritten for a custom gate if the lower-level functions `applytoall!`, `applyandadd!`, and `apply` are not sufficient.
+"""
 function applymergetruncate!(gate, prop_cache::AbstractPropagationCache, args...; kwargs...)
 
     # args is usually expected to be empty or contain a parameter for the gate
-    prop_cache = applytoall!(gate, prop_cache, args...; kwargs...)
+    # prop_cache is modified in place
+    applytoall!(gate, prop_cache, args...; kwargs...)
 
     # usually this merges from some auxillary term sum into the main term sum
     # for vector-based caches, it deduplicates within the main term sum
-    prop_cache = merge!(prop_cache; kwargs...)
+    merge!(prop_cache; kwargs...)
 
-    prop_cache = truncate!(prop_cache; kwargs...)
+    truncate!(prop_cache; kwargs...)
 
-    return prop_cache
+    return
 end
 
 """
-    applytoall!(gate, theta psum, output_psum; kwargs...)
+    applytoall!(gate, prop_cache::AbstractPropagationCache; kwargs...)
+    applytoall!(gate, prop_cache::AbstractPropagationCache, parameter; kwargs...)
 
-2nd-level function below `applymergetruncate!` that applies one gate to all Pauli strings in `psum`, moving results into `output_psum` by default.
-After this functions, Pauli strings in remaining in `psum` and `output_psum` are merged.
+1st-level function below `propagate!` that applies one gate to all terms in the main term sum `term_sum = mainsum(prop_cache)`, 
+potentially using an auxiliary term sum `aux_term_sum = auxsum(prop_cache)` in the process. 
+After this functions, all terms remaining in `term_sum` and `aux_term_sum` are merged.
 This function can be overwritten for a custom gate if the lower-level functions `applyandadd!` and `apply` are not sufficient.
-In particular, this function can be used to manipulate both `psum` and `output_psum` at the same time to reduce memory movement.
-Note that manipulating `psum` on anything other than the current Pauli string will likely lead to errors.
-See the `4-custom-gates.ipynb` for examples of how to define custom gates.
+In particular, this function can be used to manipulate both `term_sum` and `aux_term_sum` at the same time to reduce memory movement.
+Note that manipulating `term_sum` on anything other than the current term will likely lead to errors.
 """
 function applytoall!(gate, prop_cache::AbstractPropagationCache, args...; kwargs...)
     term_sum = mainsum(prop_cache)
     aux_term_sum = auxsum(prop_cache)
-    # Loop over all Pauli strings in psum and apply the gate to them.
-    for (pstr, coeff) in term_sum
-        # apply gate to one Pauli string, move new Pauli strings to aux_psum
-        applyandadd!(gate, aux_term_sum, pstr, coeff, args...; kwargs...)
+    # Loop over all terms in term_sum and apply the gate to them.
+    for (term, coeff) in term_sum
+        # apply gate to one term, move new terms to aux_term_sum
+        applyandadd!(gate, aux_term_sum, term, coeff, args...; kwargs...)
     end
 
-    # Empty psum because everything was moved into aux_term_sum. They will later be swapped.
+    # Empty term_sum because everything was moved into aux_term_sum. They will later be swapped.
     # If we want to reduce unnecessary Pauli string movement, we can overload applygatetoall!()
     empty!(term_sum)
 
-    return prop_cache
+    # by default we can already swap the term sums here
+    # merge!(prop_cache) will then likely not do anything
+    swapsums!(prop_cache)
+
+    return
 end
 
 
 """
-    applyandadd!(gate, pstr, coefficient, theta, output_psum; kwargs...)
+    applyandadd!(gate::StaticGate, output_term_sum, term, coeff; kwargs...)
+    applyandadd!(gate::ParametrizedGate, output_term_sum, term, coeff, param; kwargs...)
 
-3rd-level function below `applymergetruncate!` that applies one gate to one Pauli string in `psum`, moving results into `output_psum` by default.
+3rd-level function below `propagate!` that applies a gate to one term in `term_sum`, moving results into `output_term_sum` by default,
+which is modified in place.
 This function can be overwritten for a custom gate if the lower-level function `apply` is not sufficient. 
 This is likely the the case if `apply` is not type-stable because it does not return a unique number of outputs. 
 """
-@inline function applyandadd!(gate, output_psum, pstr, coeff, args...; kwargs...)
-    # Get the (potentially new) pauli strings and their coefficients in the form of ((pstr1, coeff1), (pstr2, coeff2), ...)
-    pstrs_and_coeffs = apply(gate, pstr, coeff, args...; kwargs...)
+@inline function applyandadd!(gate, output_term_sum, term, coeff, args...; kwargs...)
+    # Get the (potentially new) terms and their coefficients in the form of ((term1, coeff1), (term2, coeff2), ...)
+    terms_and_coeffs = apply(gate, term, coeff, args...; kwargs...)
 
-    for (new_pstr, new_coeff) in pstrs_and_coeffs
-        # Itererate over the pairs of pstr and coeff
-        # Store the new_pstr and coeff in the aux_psum, add to existing coeff if new_pstr already exists there
-        add!(output_psum, new_pstr, new_coeff)
+    for (new_term, new_coeff) in terms_and_coeffs
+        # Itererate over the pairs of term and coeff
+        # Store the new_term and coeff in the aux_term_sum, add to existing coeff if new_term already exists there
+        add!(output_term_sum, new_term, new_coeff)
     end
 
-    return output_psum
+    return
 end
 
 """
@@ -121,47 +144,48 @@ such as `(new_term, new_coeff)`, instead of `((new_term, new_coeff),)`.
 
 Example:
 ```julia
-function apply(gate::NewStaticGate, term::PauliString, coeff::Number)
-    # implementation of how Hadamard gate acts on a Pauli string
+function apply(gate::NewStaticGate, term, coeff)
+    # how some gate acts in the term and updates coeff
     ...
     return ((new_term1, new_coeff1), (new_term2, new_coeff2), ...)
 end
-function apply(gate::NewParametrizedGate, term::PauliString, coeff::Number, theta::Number)
-    # implementation of how RZ gate acts on a Pauli string
+function apply(gate::NewParametrizedGate, term, coeff, param)
+    # how some gate acts in the term and updates coeff
+    # a parameter `param` will be automatically passed if NewParametrizedGate <: ParametrizedGate
     ...
     return ((new_term1, new_coeff1), (new_term2, new_coeff2), ...)
 end
 ```
 """
-apply(gate, args...; kwargs...) = _thrownotimplemented(gate, :apply)
+@inline apply(gate, args...; kwargs...) = _thrownotimplemented(gate, :apply)
 
 
 
 
-function _promotecircandthetas(circ, thetas)
+function _promotecircandparams(circ, params)
     # if users pass a gate, we assume that thetas also requires a `[]` around it
     if circ isa Gate
         circ = [circ]
 
-        if !isnothing(thetas)
-            thetas = [thetas]
+        if !isnothing(params)
+            params = [params]
         end
     end
 
-    if isnothing(thetas)
-        thetas = []
+    if isnothing(params)
+        params = []
     end
 
-    return circ, thetas
+    return circ, params
 end
 
-function _checkcircandthetas(circ, thetas)
+function _checkcircandparams(circ, params)
     nparams = countparameters(circ)
 
-    if nparams != length(thetas)
+    if nparams != length(params)
         throw(ArgumentError(
-            "The number of thetas must match the number of parametrized gates in the circuit. " *
-            "countparameters(circ)=$nparams, length(thetas)=$(length(thetas)).")
+            "The number of parameters must match the number of parametrized gates in the circuit. " *
+            "countparameters(circ)=$nparams, length(params)=$(length(params)).")
         )
     end
 
